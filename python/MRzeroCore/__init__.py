00129095751438b9fa90c33ae1c2510f2774b9a0
J-Endres <67545816+J-Endres@users.noreply.github.com>
2023-12-14 19:23:44 +0100

    Modernized BrainWeb phantom generator

--- python/MRzeroCore/__init__.py
+++ python/MRzeroCore/__init__.py
@@ -2,12 +2,13 @@ from .sequence import PulseUsage, Pulse, Repetition, Sequence, chain
 from .phantom.voxel_grid_phantom import VoxelGridPhantom
 from .phantom.custom_voxel_phantom import CustomVoxelPhantom
 from .phantom.sim_data import SimData
-from .phantom import brainweb
+from .phantom.brainweb.generate_maps import generate_brainweb_phantoms
 from .simulation.isochromat_sim import isochromat_sim
 from .simulation.pre_pass import compute_graph, compute_graph_ext, Graph
 from .simulation.main_pass import execute_graph
 from .reconstruction import reco_adjoint
 from .pulseq.exporter import pulseq_write_cartesian
+
 # Currently not exposed directly as it is not required by typical use cases
 # and also not documented. Used internally by Sequence.from_seq_file.
 # Might re-expose later as it contains sequence plotting functionality
