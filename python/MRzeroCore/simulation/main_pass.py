bugfix: remove 2*pi from b-value calculation

python/MRzeroCore/simulation/main_pass.py
@@ -129,6 +129,7 @@ def execute_graph(graph: Graph,
         # Use the same adc phase for all coils
         adc_rot = torch.exp(1j * rep.adc_phase).unsqueeze(1)
 
+
         for dist in dists:
             # Create a list only containing ancestors that were simulated
             ancestors = list(filter(
@@ -154,9 +155,9 @@ def execute_graph(graph: Graph,
             dist_traj = dist.kt_vec + trajectory
 
             # Diffusion
-            k2 = dist_traj[:, :3] * 2 * np.pi
+            k2 = dist_traj[:, :3]
             k1 = torch.empty_like(k2)  # Calculate k-space at start of event
-            k1[0, :] = dist.kt_vec[:3] * 2 * np.pi
+            k1[0, :] = dist.kt_vec[:3]
             k1[1:, :] = k2[:-1, :]
             # Integrate over each event to get b factor (lin. interp. grad)
             b = 1/3 * dt * (k1**2 + k1*k2 + k2**2).sum(1)
@@ -202,7 +203,7 @@ def execute_graph(graph: Graph,
                 dist.mag = dist.mag * r2 * diffusion[-1, :]
                 dist.kt_vec = dist_traj[-1]
             else:  # z or z0
-                k = torch.linalg.vector_norm(dist.kt_vec[:3] * 2 * np.pi)
+                k = torch.linalg.vector_norm(dist.kt_vec[:3])
                 diffusion = torch.exp(-1e-9 * data.D * total_time * k**2)
                 dist.mag = dist.mag * r1 * diffusion
             if dist.dist_type == 'z0':