4c0f4d47be6411e57b3738a5f6b489e6f1443e2e
Felix Landmeyer <f.landmeyer@fz-juelich.de>
2025-05-22 11:39:44 +0200

    Direct import of sig_to_mrd with MRzeroCore

--- python/MRzeroCore/__init__.py
+++ python/MRzeroCore/__init__.py
@@ -1,4 +1,5 @@
 import numpy
+
 if not hasattr(numpy, "int"):
     numpy.int = int
 if not hasattr(numpy, "float"):
@@ -6,14 +7,15 @@ if not hasattr(numpy, "float"):
 if not hasattr(numpy, "complex"):
     numpy.complex = complex
 
-from .sequence import PulseUsage, Pulse, Repetition, Sequence, chain
-from .phantom.voxel_grid_phantom import VoxelGridPhantom
+from . import util
+from .phantom.brainweb import generate_brainweb_phantoms
 from .phantom.custom_voxel_phantom import CustomVoxelPhantom
 from .phantom.sim_data import SimData
-from .phantom.brainweb import generate_brainweb_phantoms
-from .simulation.isochromat_sim import isochromat_sim
-from .simulation.pre_pass import compute_graph, compute_graph_ext, Graph
-from .simulation.main_pass import execute_graph
-from .reconstruction import reco_adjoint
+from .phantom.voxel_grid_phantom import VoxelGridPhantom
 from .pulseq.exporter import pulseq_write_cartesian
-from . import util
+from .reconstruction import reco_adjoint
+from .sequence import Pulse, PulseUsage, Repetition, Sequence, chain
+from .simulation.isochromat_sim import isochromat_sim
+from .simulation.main_pass import execute_graph
+from .simulation.pre_pass import Graph, compute_graph, compute_graph_ext
+from .simulation.sig_to_mrd import sig_to_mrd
