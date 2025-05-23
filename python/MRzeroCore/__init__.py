fbbc4e72a3043cc1bbdedfec73d6fe0f5baabf72
Felix Landmeyer <f.landmeyer@fz-juelich.de>
2025-05-23 15:44:05 +0200

    Revert "Direct import of sig_to_mrd with MRzeroCore"
    This reverts commit 4c0f4d47be6411e57b3738a5f6b489e6f1443e2e.

--- python/MRzeroCore/__init__.py
+++ python/MRzeroCore/__init__.py
@@ -1,5 +1,4 @@
 import numpy
-
 if not hasattr(numpy, "int"):
     numpy.int = int
 if not hasattr(numpy, "float"):
@@ -7,15 +6,15 @@ if not hasattr(numpy, "float"):
 if not hasattr(numpy, "complex"):
     numpy.complex = complex
 
-from . import util
-from .phantom.brainweb import generate_brainweb_phantoms
+from .sequence import PulseUsage, Pulse, Repetition, Sequence, chain
+from .phantom.voxel_grid_phantom import VoxelGridPhantom
 from .phantom.custom_voxel_phantom import CustomVoxelPhantom
 from .phantom.sim_data import SimData
-from .phantom.voxel_grid_phantom import VoxelGridPhantom
-from .pulseq.exporter import pulseq_write_cartesian
-from .reconstruction import reco_adjoint
-from .sequence import Pulse, PulseUsage, Repetition, Sequence, chain
+from .phantom.brainweb import generate_brainweb_phantoms
 from .simulation.isochromat_sim import isochromat_sim
+from .simulation.pre_pass import compute_graph, compute_graph_ext, Graph
 from .simulation.main_pass import execute_graph
-from .simulation.pre_pass import Graph, compute_graph, compute_graph_ext
 from .simulation.sig_to_mrd import sig_to_mrd
+from .reconstruction import reco_adjoint
+from .pulseq.exporter import pulseq_write_cartesian
+from . import util
