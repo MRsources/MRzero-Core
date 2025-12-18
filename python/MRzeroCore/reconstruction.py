sign fix

python/MRzeroCore/reconstruction.py
@@ -87,12 +87,11 @@ def reco_adjoint(signal: torch.Tensor,
     ], dim=1).t()
 
     NCoils = signal.shape[1]
-    # assert NCoils == 1, "reconstruct currently does not support multicoil"
 
     # (Samples, 3) x (3, Voxels)
     phase = kspace[:, :3] @ voxel_pos
     # (Samples, Voxels): Rotation of all voxels at every event
-    rot = torch.exp(-2j*pi * phase)  # Matches definition of forward DFT
+    rot = torch.exp(2j*pi * phase)  # Matches definition of backward DFT
 
     NCoils = signal.shape[1]
 

python/MRzeroCore/simulation/main_pass.py
@@ -236,7 +236,7 @@ def execute_graph(graph: Graph,
 
                 T2 = torch.exp(-trajectory[adc, 3:] / torch.abs(data.T2))
                 T2dash = torch.exp(-torch.abs(adc_dist_traj[:, 3:]) / torch.abs(data.T2dash))
-                rot = torch.exp(2j * np.pi * (
+                rot = torch.exp(-2j * np.pi * (
                     (adc_dist_traj[:, 3:] * data.B0) +
                     (adc_dist_traj[:, :3] @ data.voxel_pos.T) +
                     adc_motion_phase